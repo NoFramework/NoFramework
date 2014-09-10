c4ff3ec2344eb5533c6c2fa5e10ef84e717edaaa
Roman Zaykin <rzaykin@gmail.com>
2014-09-10 07:10:01 +0400

    refactored and simplified error handler

--- ErrorHandler.php
+++ ErrorHandler.php
@@ -7,46 +7,45 @@
  * @link http://noframework.com
  */
 
-namespace NoFramework\Error;
+namespace NoFramework;
 
-class Handler
+class ErrorHandler
 {
     protected $error_types = -1;
-    protected $is_preload_exception = true;
 
-    public function __construct($error_types = false)
+    const ERROR = 5;
+    const WARNING = 4;
+    const NOTICE = 3;
+    const STRICT = 2;
+    const DEPRECATED = 1;
+
+    public function __construct($error_types = -1)
     {
-        if (false !== $error_types) {
-            $this->error_types = $error_types;
-        }
+        $this->error_types = $this->error_types;
     }
 
-    public function getErrorClass()
+    public function __invoke($errno, $errstr, $errfile, $errline)
     {
-        $return = [];
+        if (0 !== error_reporting()) { // respect @
+            $severity = [
+                E_RECOVERABLE_ERROR => [self::ERROR, 'Error'],
+                E_USER_ERROR => [self::ERROR, 'Error'],
+                E_WARNING => [self::WARNING, 'Warning'],
+                E_USER_WARNING => [self::WARNING, 'Warning'],
+                E_NOTICE => [self::NOTICE, 'Notice'],
+                E_USER_NOTICE => [self::NOTICE, 'Notice'],
+                E_STRICT => [self::STRICT, 'Strict'],
+                E_DEPRECATED => [self::DEPRECATED, 'Deprecated'],
+                E_USER_DEPRECATED => [self::DEPRECATED, 'Deprecated'],
+            ][$errno];
 
-        foreach ([
-            E_USER_ERROR => 'Recoverable',
-            E_RECOVERABLE_ERROR => 'Recoverable',
-            E_USER_WARNING => 'Warning',
-            E_WARNING => 'Warning',
-            E_USER_NOTICE => 'Notice',
-            E_NOTICE => 'Notice',
-            E_STRICT => 'Strict',
-            E_USER_DEPRECATED => 'Deprecated',
-            E_DEPRECATED => 'Deprecated'
-        ] as $errno => $errclass) {
-            $return[$errno] = __NAMESPACE__ . '\Exception\\' . $errclass;
-        }
-
-        return $return;
-    }
-
-    public function __invoke($errno, $errstr, $errfile, $errline, $errcontext)
-    {
-        if (0 !== error_reporting()) {
-            $class = $this->getErrorClass()[$errno];
-            throw new $class($errno, $errstr, $errfile, $errline, $errcontext);
+            throw new \ErrorException(
+                sprintf('[%s] %s', $severity[1], $errstr),
+                $errno,
+                $severity[0],
+                $errfile,
+                $errline
+            );
         }
 
         return true;
@@ -54,23 +53,9 @@ class Handler
 
     public function register()
     {
-        if ($this->is_preload_exception) {
-            foreach (array_unique($this->getErrorClass()) as $class) {
-                spl_autoload_call($class);
-            }
-        }
+        set_error_handler($this, $this->error_types);
 
-        return set_error_handler($this, $this->error_types);
-    }
-
-    public static function restorePrevious()
-    {
-        return restore_error_handler();
-    }
-
-    public static function restoreBuiltin()
-    {
-        return set_error_handler(null);
+        return $this;
     }
 }
 
