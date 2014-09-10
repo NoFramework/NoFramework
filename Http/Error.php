751a0bbc0a3fe7893f4f6b1c3229bcbe75bbf4f3
Roman Zaykin <rzaykin@gmail.com>
2014-09-10 06:56:00 +0400

    refactored error status exception

--- Http/Error.php
+++ Http/Error.php
@@ -7,53 +7,56 @@
  * @link http://noframework.com
  */
 
-namespace NoFramework\Http\Exception;
+namespace NoFramework\Http;
 
-class ErrorStatus extends \RuntimeException
+class Error extends \Exception
 {
-    protected $messages = [
-        '400' => 'Bad Request',
-        '401' => 'Unauthorized',
-        '402' => 'Payment Required',
-        '403' => 'Forbidden',
-        '404' => 'Not Found',
-        '405' => 'Method Not Allowed',
-        '406' => 'Not Acceptable',
-        '407' => 'Proxy Authentication Required',
-        '408' => 'Request Timeout',
-        '409' => 'Conflict',
-        '410' => 'Gone',
-        '411' => 'Length Required',
-        '412' => 'Precondition Failed',
-        '413' => 'Request Entity Too Large',
-        '414' => 'Request-URI Too Long',
-        '415' => 'Unsupported Media Type',
-        '416' => 'Requested Range Not Satisfiable',
-        '417' => 'Expectation Failed',
-        '500' => 'Internal Server Error',
-        '501' => 'Method Not Implemented',
-        '502' => 'Bad Gateway',
-        '503' => 'Service Unavailable',
-        '504' => 'Gateway Timeout',
-        '505' => 'HTTP Version Not Supported',
+    protected $message = [
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        406 => 'Not Acceptable',
+        407 => 'Proxy Authentication Required',
+        408 => 'Request Timeout',
+        409 => 'Conflict',
+        410 => 'Gone',
+        411 => 'Length Required',
+        412 => 'Precondition Failed',
+        413 => 'Request Entity Too Large',
+        414 => 'Request-URI Too Long',
+        415 => 'Unsupported Media Type',
+        416 => 'Requested Range Not Satisfiable',
+        417 => 'Expectation Failed',
+        500 => 'Internal Server Error',
+        501 => 'Method Not Implemented',
+        502 => 'Bad Gateway',
+        503 => 'Service Unavailable',
+        504 => 'Gateway Timeout',
+        505 => 'HTTP Version Not Supported',
     ];
 
-    protected $parameters;
+    protected $option = [];
 
-    public function __construct($code, $parameters = [],
-        \Exception $previous = null)
+    public function __construct($code, $option = [], $previous = null)
     {
-        $this->parameters = $parameters;
+        $message = &$option['message'];
+        unset($option['message']);
 
-        parent::__construct(isset($parameters['message'])
-            ? $parameters['message']
-            : (isset($this->messages[$code]) ? $this->messages[$code] : '')
-        , $code, $previous);
+        $this->option = $option;
+
+        if (!$message and isset($this->message[$code])) {
+            $message = $this->message[$code];
+        }
+
+        parent::__construct($message, $code, $previous);
     }
 
-    final public function getParameters()
+    public function option($name)
     {
-        return $this->parameters;
+        return isset($this->option[$name]) ? $option[$name] : null;
     }
 }
 
