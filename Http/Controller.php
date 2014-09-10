b8c4e0bd17ffc74a1cb98da2da5825b4364dc242
Roman Zaykin <rzaykin@gmail.com>
2014-09-10 06:54:26 +0400

    refactored http controller

--- Http/Controller.php
+++ Http/Controller.php
@@ -1,100 +1,227 @@
 <?php
+/**
+ * NoFramework
+ *
+ * @author Roman Zaykin <roman@noframework.com>
+ * @license http://www.opensource.org/licenses/mit-license.php MIT
+ * @link http://noframework.com
+ */
+
 namespace NoFramework\Http;
-use NoFramework\Http\Exception\ErrorStatus;
+
+use NoFramework\Template\Twig as Template;
+use NoFramework\Model\Collection as Model;
 
 class Controller extends \NoFramework\Factory
 {
-    protected function model($name = false)
+    protected function __property_template()
     {
-        $model = $this->reuse('model');
-
-        if ($name) {
-            $model = $model->reuse($name);
-        }
-
-        return $model;
+        return $this->setState(new Template, [
+            'path' => dirname($_SERVER['SCRIPT_FILENAME']) . '/template'
+        ]);
     }
 
     protected function __property_request()
     {
-        return $this->reuse('request');
+        return new Request;
     }
 
     protected function __property_session()
     {
-        return $this->reuse('session');
+        return $this->is_cli ? new \ArrayObject : new Session;
     }
 
-    protected function &localSession()
+    protected function __property_is_cli()
     {
-        return $this->session['local'][$this->localId()];
+        return 'cli' === php_sapi_name();
     }
 
-    protected function __property_view()
+    protected function __property_path()
     {
-        return $this->reuse('view');
+        return '/';
     }
 
-    protected function __action_index($option = [])
+    protected function __property_model()
     {
-        $template = $this->localId(DIRECTORY_SEPARATOR);
-
-        if ($this->view->hasTemplate($template)) {
-            return $this->view($template);
-        } else {
-            throw new ErrorStatus(404);
-        }
+        return new Model;
     }
 
-    protected function view($template = false, $option = [])
+    public function __call($method, $argument)
     {
-        if (is_string($option)) {
-            $option = ['template' => $option];
+        if ($this->isAction($method)) {
+            $option = &$argument[0];
+
+            return $this->normalizeView(
+                $this->{'__action_' . $method}($option)
+            );
         }
 
-        if (!isset($option['filters']['url'])) {
-            $option['filters']['url'] = function ($query) {
-                return $this->request->getUrl($query);
-            };
-        }
-
-        return $this->view[$option];
+        return parent::__call($method, $argument);
     }
 
-    public function route($path)
+    public function __filter_url($query)
     {
-        $path = str_replace('.', '/', trim($path, '/'));
+        return $this->url($query);
+    }
+
+    public function isAction($action)
+    {
+        return method_exists($this, '__action_' . $action);
+    }
+
+    public function route($path, $method)
+    {
+        $path = trim($path, '/');
         $action = $path ? str_replace('/', '_', $path) : 'index';
         $next = strtok($path, '/');
+        $next_path = strtok('');
 
         if ($this->isAction($action)) {
-            return [
+            return (object)[
                 'controller' => $this,
                 'action' => $action,
+                'correct_path' => $this->path,
             ];
-        } elseif (isset($this->$next) and $this->$next instanceof self) {
-            return $this->$next->route(strtok(''));
+        } elseif (
+            $next and
+            isset($this->$next) and
+            $this->$next instanceof self
+        ) {
+            return $this->$next->route($next_path, $method);
         }
 
         return false;
     }
 
-    protected function isAction($action, $option = [])
+    public function url($query)
     {
-        return method_exists($this, '__action_' . $action);
+        return $this->request->url($query, $this->path);
     }
 
-    public function __call($method, $argument) {
-        if ($this->isAction($method)) {
-            return $this->{'__action_' . $method}(isset($argument[0]) ? $argument[0] : null);
+    protected function view($state = [], $data = [])
+    {
+        return new View($state, $data);
+    }
 
-        } else {
-            trigger_error(sprintf(
-                'Call to undefined method %s::%s()',
-                static::class,
-                $method
-            ), E_USER_ERROR);
+    protected function normalizeView($view)
+    {
+        if ($view instanceof View) {
+            return $view;
         }
+
+        $state = [];
+        $data = [];
+
+        if (!isset($view)) {
+            $state['is_silent'] = true;
+
+        } elseif (is_bool($view)) {
+            $data['success'] = $view;
+
+        } elseif (is_callable($view)) {
+            $state['template'] = $view;
+
+        } elseif (is_string($view)) {
+            $state['template'] = $view;
+            $state['content_type'] = 'text/plain';
+
+        } elseif (is_array($view)) {
+            $data = $view;
+
+            $state['template'] = &$data['template'];
+            unset($data['template']);
+
+            if (is_string($state['template'])) {
+                $state['template'] = $this->template($state['template']);
+            }
+        } else {
+            $state['template'] = $view;
+        }
+
+        return $this->view($state, $data);
+    }
+    
+    protected function template($name, $filters = [], $type = 'file')
+    {
+        foreach (get_class_methods($this) as $method) {
+            if (0 === strpos($method, '__filter_')) {
+                $filter = substr($method, strlen('__filter_'));
+                $filters[$filter] = [$this, $method];
+
+            } elseif (0 === strpos($method, '__widget_')) {
+                $filter = substr($method, strlen('__widget_'));
+
+                $filters['widget_' . $filter] = [
+                    [$this, $method],
+                    'is_safe' => ['html']
+                ];
+            }
+        }
+
+        return $this->template->load($name, $filters, $type);
+    }
+
+    protected function error($code, $option = [])
+    {
+        throw new Error($code, $option);
+    }
+
+    protected function redirect($location, $code = 302)
+    {
+        throw new Redirect($location, $code);
+    }
+
+    protected function __resolve_new($value = null, $as = null)
+    {
+        $auto = $this->autoNamespace($as);
+
+        if (!class_exists($auto)) {
+            $auto = $this->use->normalizeClass('Controller');
+        }
+
+        $class =
+            $this->popClass($value) ?:
+            (class_exists($auto) ? $auto : self::class)
+        ;
+
+        if (is_a($class, self::class, true)) {
+            if ($as and 0 !== strpos($as, '.')) {
+                $value['path'] =
+                    $this->path . str_replace('.' , '/', $as) . '/';
+            }
+
+            $value += [
+                'template' => $this->{'$template'},
+                'request' => $this->{'$request'},
+                'session' => $this->{'$session'},
+                'model' => $this->{'$model'},
+            ];
+        }
+
+        $value['class'] = '\\' . $class;
+
+        return parent::__resolve_new($value, $as);
+    }
+
+    protected function __action_index($option = [])
+    {
+        $path = (
+            isset($this->template->search_path)
+            ? $this->template->search_path
+            : ''
+        ) . $this->path;
+
+        $template = $path . 'index';
+
+        if (!$this->template->exists($template)) {
+            $template = substr($path, 0, -1);
+        }
+
+        if (!$this->template->exists($template)) {
+            $this->error(404);
+        }
+
+        return ['template' => $template];
     }
 }
 
