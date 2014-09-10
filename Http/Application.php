2990849ee588c009955adbf63a49a1fb00912e41
Roman Zaykin <rzaykin@gmail.com>
2014-09-10 06:53:30 +0400

    refactored http front controller

--- Http/Application.php
+++ Http/Application.php
@@ -8,193 +8,63 @@
  */
 
 namespace NoFramework\Http;
-use NoFramework\Http\Exception\ErrorStatus;
-use NoFramework\Http\Exception\Redirect;
 
-abstract class Application
+class Application extends Controller
 {
-    use \NoFramework\MagicProperties;
-
-    protected $redirect_index;
-    protected $route;
-    protected $controller;
-
-    protected function __property_request()
-    {
-        return new Request;
-    }
-
     protected function __property_response()
     {
         return new Response;
     }
 
-    protected function __property_view()
-    {
-        return new View\Factory;
-    }
-
-    protected function __property_session()
-    {
-        return (new Session)->start();
-    }
-
-    protected function __property_display_errors()
-    {
-        return ini_get('display_errors');
-    }
-
-    protected function __property_is_cli()
-    {
-        return 'cli' === php_sapi_name();
-    }
-
-    protected function respond($view = null, $status = 200, $headers = [])
-    {
-        if (is_string($view)) {
-            $this->view[null]->respondHeaders($this->response, [
-                'status' => $status,
-                'headers' => $headers,
-            ]);
-
-            $this->response->payload($view);
-        } else {
-            $this->view[$view]->respond($this->response, [
-                'status' => $status,
-                'headers' => $headers,
-                'blocks' =>
-                    is_string($this->request['blocks'])
-                    ? array_combine(
-                        explode(',', $this->request['blocks']),
-                        explode(',', $this->request['blocks'])
-                    )
-                    : $this->request['blocks']
-            ]);
-        }
-
-        return $this;
-    }
-
-    protected function respondError($exception)
-    {
-        $status = $exception->getCode();
-        $parameters = $exception->getParameters();
-
-        return $this->respond(
-            sprintf(
-                '%1$s - %2$s' . PHP_EOL .
-                'method: %3$s' . PHP_EOL .
-                'url: %4$s' . PHP_EOL .
-                '%5$s',
-                $status,
-                $exception->getMessage(),
-                $this->request->method,
-                $this->request,
-                $this->display_errors
-                    ? $exception->getPrevious()
-                    : null
-            ),
-            $status,
-            isset($parameters['headers']) ? $parameters['headers'] : []
-        );
-    }
-
-    public function route($method, $path)
-    {
-        foreach ((array)$this->route as $route) {
-            if (is_string($route)) {
-                $route = ['path' => $route];
-            }
-
-            $route = array_merge([
-                'method' => ['GET', 'POST'],
-                'action' => 'index',
-                'controller' =>
-                    str_replace('/', '.', trim($route['path'], '/')),
-            ], $route);
-
-            if (
-                $path === $route['path'] and
-                in_array($method, (array)$route['method']) and
-                $controller = $this->controller->reuse($route['controller'])
-            ) {
-                return [
-                    'controller' => $this->controller,
-                    'action' => $route['action'],
-                ];
-            }
-        }
-
-        return false;
-    }
-
-    protected function main($option)
-    {
-        $path = $this->request->path;
-        $method = $this->request->method;
-
-        if ('/' === $path and $this->redirect_index) {
-            throw new Redirect($this->request->getUrl([
-                'path' => $this->redirect_index,
-                'is_only' => true,
-            ]));
-        }
-
-        $route = $this->route($method, $path);
-
-        if (!$route and isset($this->controller)) {
-            $route = $this->controller->route($path);
-        }
-
-        if ($route) {
-            if ('GET' === $method and '/' !== substr($path, -1)) {
-                throw new Redirect($this->request->getUrl($path . '/'));
-            }
-
-            $this->respond($route['controller']->{$route['action']}($option));
-        } else {
-            throw new ErrorStatus(404);
-        }
-    }
-
     public function start($option = [])
     {
-        if (isset($_SERVER['argv'][1]) and !isset($option['request'])) {
+        $option = is_string($option) ? ['request' => $option] : $option;
+
+        if ($this->is_cli and isset($_SERVER['argv'][1])) {
             $option['request'] = $_SERVER['argv'][1];
         }
 
-        if (is_string($option)) {
-            $option = ['request' => $option];
+        if ($request = &$option['request']) {
+            $this->__property['request'] = new Request($request);
         }
 
-        if (isset($option['request'])) {
-            $this->__property['request'] = new Request($option['request']);
+        unset($option['request']);
+
+        if ($this->session instanceof Session) {
+            $this->session->start();
         }
 
-        if (isset($option['session'])) {
-            foreach ($option['session'] as $key => $value) {
+        if ($session = &$option['session']) {
+            foreach ($session as $key => $value) {
                 $this->session[$key] = $value;
             }
         }
 
+        unset($option['session']);
+
         $jobs = [];
 
         try {
-            $this->main([
-                'emit_jobs' => function ($job) use (&$jobs) {
-                    $jobs[] = $job;
-                },
-            ]);
+            $option['job'] = function ($job) use (&$jobs) {
+                $jobs[] = $job;
+            };
+
+            $this->main($option);
 
         } catch (Redirect $e) {
             $this->response->redirect($e->getLocation(), $e->getCode());
 
-        } catch (ErrorStatus $e) {
+        } catch (Error $e) {
             $this->respondError($e);
 
         } catch (\Exception $e) {
-            $this->respondError(new ErrorStatus(500, [], $e));
+            $this->respondError(new Error(500, [], $e));
 
+        } finally {
+            if ($this->session instanceof Session) {
+                $this->session->clear();
+                $this->session->writeClose();
+            }
         }
 
         flush();
@@ -203,13 +73,57 @@ abstract class Application
             fastcgi_finish_request();
         }
 
-        if ($this->session instanceof Session) {
-            $this->session->writeClose();
-        }
-
         foreach ($jobs as $job) {
             $job();
         }
+    }
+
+    protected function respond($view)
+    {
+        return $view->respond($this->response);
+    }
+
+    protected function main($option)
+    {
+        $path = $this->request->path;
+        $method = $this->request->method;
+
+        if ($route = $this->route($path, $method)) {
+            if (
+                'GET' === $method and
+                $route->correct_path and
+                $route->correct_path !== $path
+            ) {
+                $this->redirect($this->request->url($route->correct_path), 301);
+            }
+
+            $this->respond($route->controller->{$route->action}($option));
+
+        } else {
+            $this->error(404);
+        }
+    }
+
+    protected function respondError($exception)
+    {
+        $status = $exception->getCode();
+
+        return $this->view([
+            'status' => $status,
+            'content_type' => 'text/plain',
+            'headers' => $exception->option('headers') ?: [],
+            'template' => sprintf(
+                '%1$s - %2$s' . PHP_EOL .
+                'method: %3$s' . PHP_EOL .
+                'url: %4$s' . PHP_EOL .
+                '%5$s',
+                $status,
+                $exception->getMessage(),
+                $this->request->method,
+                $this->request,
+                ini_get('display_errors') ? $exception->getPrevious() : null
+            ),
+        ])->respond($this->response);
     }
 }
 
