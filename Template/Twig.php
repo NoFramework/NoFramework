88a46e22e6c0977a4df5e9e663021c3f424c2da8
Roman Zaykin <rzaykin@gmail.com>
2014-09-10 07:07:24 +0400

    template is stand alone entity as it was earlier, and refactored

--- Template/Twig.php
+++ Template/Twig.php
@@ -7,134 +7,87 @@
  * @link http://noframework.com
  */
 
-namespace NoFramework\Http\View;
+namespace NoFramework\Template;
 
-class Factory implements \ArrayAccess
+class Twig
 {
-    use \NoFramework\ArrayShortcut;
+    use \NoFramework\Magic;
 
-    protected $charset = 'utf-8';
-    protected $template_extension = '.html.twig';
-    protected $template_path = [];
+    protected $path;
+    protected $extension = '.html.twig';
 
-    /**
-     * cache
-     * debug
-     * auto_reload
-     * strict_variables
-     * base_template_class
-     * autoescape
-     * optimizations
-     */
-    protected $twig_options = [];
+    protected $debug = false;
+    protected $charset = 'UTF-8';
+    protected $base_template_class = 'Twig_Template';
+    protected $strict_variables = false;
+    protected $autoescape = 'html';
+    protected $cache = false;
+    protected $auto_reload = null;
+    protected $optimizations = -1;
 
-    /**
-     * is_hex_quot
-     * is_hex_tag
-     * is_hex_amp
-     * is_hex_apos
-     * is_numeric_check
-     * is_pretty_print
-     * is_unescaped_slashes
-     * is_force_object
-     * is_unescaped_unicode
-     * depth (default: 512)
-     */
-    protected $json_options = [];
-
-    protected function loadTemplate($name, $filters = [])
+    protected function __property_file_loader()
     {
-        $out = new \Twig_Environment(
-            new \Twig_Loader_Filesystem($this->template_path),
-            array_merge(
-                ['charset' => $this->charset],
-                $this->twig_options
-            )
-        );
-        
-        foreach ($filters as $filter_name => $filter) {
-            $out->addFilter(new \Twig_SimpleFilter(
-                $filter_name,
-                $filter
-            ));
-        }
-
-        $out->addFilter(new \Twig_SimpleFilter(
-            'dump',
-            function ($object) {
-                return '<pre>' . print_r($object, true) . '</pre>';
-            },
-            ['is_safe' => ['html']]
-        ));
-
-        return $out->loadTemplate($name . $this->template_extension);
+        return new \Twig_Loader_Filesystem($this->path);
     }
 
-    /**
-     * template
-     * filters
-     * data
-     */
-    public function get($data = [])
+    protected function __property_string_loader()
     {
-        if ($data instanceof Item) {
-            return $data;
-        }
+        return new \Twig_Loader_String();
+    }
 
-        $state = [
-            'json_options' => array_reduce([
-                ['is_hex_quot', JSON_HEX_QUOT],
-                ['is_hex_tag', JSON_HEX_TAG],
-                ['is_hex_amp', JSON_HEX_AMP],
-                ['is_hex_apos', JSON_HEX_APOS],
-                ['is_numeric_check', JSON_NUMERIC_CHECK],
-                ['is_pretty_print', JSON_PRETTY_PRINT],
-                ['is_unescaped_slashes', JSON_UNESCAPED_SLASHES],
-                ['is_force_object', JSON_FORCE_OBJECT],
-                ['is_unescaped_unicode', JSON_UNESCAPED_UNICODE],
-            ], function ($out, $item) {
-                if (isset($this->json_options[$item[0]]) and $this->json_options[$item[0]]) {
-                    $out |= $item[1];
-                    return $out;
-                }
-            }, 0),
-            'charset' => $this->charset,
+    public function exists($template)
+    {
+        return $this->file_loader->exists($template . $this->extension);
+    }
+
+    public function load($template, $filters = [], $type = 'file')
+    {
+        $options = [
+            'debug',
+            'charset',
+            'base_template_class',
+            'strict_variables',
+            'autoescape',
+            'cache',
+            'auto_reload',
+            'optimizations',
         ];
 
-        if (isset($this->json_options['depth'])) {
-            $state['json_depth'] = $this->json_options['depth'];
+        $options = array_map(function ($property) {
+            return $this->$property;
+        }, array_combine($options, $options));
+
+        $environment = new \Twig_Environment(
+            $this->{$type . '_loader'},
+            $options
+        );
+
+        if ($options['debug']) {
+            $environment->addExtension(new \Twig_Extension_Debug);
         }
 
-        if (is_string($data)) {
-            $state['template'] = $this->loadTemplate($data);
-        } else {
-            if (isset($data['template'])) {
-                $state['template'] = $this->loadTemplate(
-                    $data['template'],
-                    isset($data['filters']) ? $data['filters'] : []
-                );
+        $environment->addExtension(new \Twig_Extension_StringLoader);
 
-                unset($data['filters']);
+        foreach ($filters as $name => $filter) {
+            $options = [];
+
+            if (
+                !is_callable($filter) and
+                is_array($filter) and
+                is_callable(reset($filter))
+            ) {
+                $options = $filter;
+                $filter = array_shift($options);
             }
 
-            $state['data'] = $data;
+            $environment->addFilter(
+                new \Twig_SimpleFilter($name, $filter, $options)
+            );
         }
 
-        return new Item($state);
-    }
-
-    public function hasTemplate($template)
-    {
-        foreach ((array)$this->template_path as $template_path) {
-            if (is_file(
-                $template_path . DIRECTORY_SEPARATOR .
-                $template . $this->template_extension
-            )) {
-                return true;
-            }
-        }
-
-        return false;
+        return $environment->loadTemplate(
+            $template . ('file' === $type ? $this->extension : '')
+        );
     }
 }
 
