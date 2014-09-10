fbb65c69055d68b2a2d89d6ff24f2dc8a33424ce
Roman Zaykin <rzaykin@gmail.com>
2014-09-10 07:03:02 +0400

    refactored service application

--- Service/Application.php
+++ Service/Application.php
@@ -9,52 +9,113 @@
 
 namespace NoFramework\Service;
 
+declare(ticks = 1);
+
 class Application
 {
-    use \NoFramework\MagicProperties;
+    use \NoFramework\Magic;
 
     protected $pidfile;
-    protected $timeout = 3600;
-    protected $action;
 
-    protected function __property_run()
+    protected function __property_log()
     {
-        $run = $_SERVER['argv'];
-        array_shift($run);
-        return $run;
+        return new Log;
     }
 
-    protected function main()
+    protected function __property_error_log()
     {
-        foreach ($this->run as $action) {
-            try {
-                $this->action->$action->run();
-            } catch (Exception\Stop $e) {
-            } catch (\Exception $e) {
-                $this->action->$action->error_log->write($e);
-            } 
+        return new ErrorLog;
+    }
+
+    public function start()
+    {
+        if ($this->workingPid()) {
+            return;
         }
-    }
 
-    public function start($main = false)
-    {
         if ($this->pidfile) {
-            $pidfile = $this->pidfile;
-
-            if (is_string($pidfile)) {
-                $pidfile = new PidFile;
-                $pidfile->path = $this->pidfile;
+            if (is_file($this->pidfile)) {
+                unlink($this->pidfile);
             }
 
-            if (!$pidfile->check($this->timeout)) {
-                $pidfile->write();
-                $result = $this->main();
-                $pidfile->delete();
-                return $result;
+            file_put_contents($this->pidfile, $this->pid() . PHP_EOL);
+        }
+
+        try {
+            foreach (get_class_methods($this) as $method) {
+                if (0 === strpos($method, '__signal_')) {
+                    pcntl_signal(
+                        constant('SIG' . strtoupper(
+                            substr($method, strlen('__signal_'))
+                        )),
+                        [$this, $method]
+                    );
+                }
             }
-        } else {
-            return $this->main();
+
+            $this->log('start');
+            $this->main();
+            $this->log('done');
+
+        } catch (Stop $stop) {
+            if ($message = $stop->getMessage()) {
+                $this->log($message);
+            }
+
+        } catch (\Exception $e) {
+            $this->error_log($e);
+
+        } finally {
+            if ($this->workingPid() === $this->pid()) {
+                unlink($this->pidfile);
+            }
         }
     }
+
+    protected function stop($message = 'stop')
+    {
+        throw new Stop($message);
+    }
+
+    protected function log($message, $type = false)
+    {
+        $this->log->write($message, $type);
+    }
+
+    protected function error_log($message, $type = false)
+    {
+        $this->error_log->write($message, $type);
+    }
+
+    protected function workingPid()
+    {
+        return
+            (
+                $this->pidfile and
+                is_file($this->pidfile) and
+                $pid = (int)file_get_contents($this->pidfile) and
+                (posix_kill($pid, 0) or 3 !== posix_get_last_error())
+            )
+            ? $pid
+            : false
+        ;
+    }
+
+    protected function pid()
+    {
+        return (int)posix_getpid();
+    }
+
+    public function __signal_int()
+    {
+        $this->stop('break');
+    }
+
+    public function __signal_term()
+    {
+        $this->stop('terminate');
+    }
+
+    protected function main() {}
 }
 
